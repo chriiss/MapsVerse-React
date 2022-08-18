d1321918f98314fa049e8981ec6e81f26eb8f63a
chriiss <christopher.kumla@gmail.com>
2022-08-18 18:36:45 +0200

    add itinary and geocode ok

--- src/app/components/maps/routingMachine/RoutingMachine.js
+++ src/app/components/maps/routingMachine/RoutingMachine.js
@@ -3,25 +3,33 @@ import { createControlComponent } from "@react-leaflet/core";
 import "leaflet-routing-machine";
 import "/node_modules/leaflet-routing-machine/dist/leaflet-routing-machine.css";
 
-const createRoutineMachineLayer = () => {
+const createRoutineMachineLayer = (props) => {
   const geocoder = L.Control.Geocoder.nominatim();
+  const icon = L.icon({
+    "iconUrl": require("../../../assets/marker.png"),
+    "iconSize": [42,38]
+});
   const instance = L.Routing.control({
-    waypoints: [
-      L.latLng(48.85337, 2.35034),
-      L.latLng(46.39248, 0.16387)
-    ],
+    geocoder: geocoder,
     lineOptions: {
       styles: [{ color: "rgb(30, 96, 250)", weight: 4 }]
     },
     show: true,
     collapsible: true,
-    addWaypoints: false,
     routeWhileDragging: true,
-    draggableWaypoints: true,
-    fitSelectedRoutes: true,
-    showAlternatives: false,
-    geocoder: geocoder,
+    router: new L.Routing.osrmv1({
+      language: "fr",
+      profile: "car"
+    }),
+    createMarker: function (i, waypoint, n) {
+      const marker = L.marker(waypoint.latLng, {
+        draggable: true,
+        icon: icon
+      });
+      return marker;
+    }
   });
+
   return instance;
 };
 
